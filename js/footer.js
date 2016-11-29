ca8941ff1c4ef0da6f514a1602a0b4f749d47e0b
Katie <katie.jergens@gmail.com>
2016-11-28 21:58:52 -0500

    twitter footer

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 var footer = "<br>" +
    "<p><a href=\"https://www.facebook.com/kateinbrooklyn/?fref=nf\" target=\"blank\">Facebook</a></p>" +
    "<p><a href=\"https://www.linkedin.com/in/katie-jergens-29695aa\" target=\"blank\">LinkedIn</a></p>" +
-    "<p><a href=\"https://twitter.com/KatieJergens\" target=\"blank\">Twitter</a></p>" +  
+    "<p><a href=\"https://twitter.com/KatieInBrooklyn\" target=\"blank\">Twitter</a></p>" +  
     "<br><br>Site designed & developed by Katie Jergens";
 
 $(document).ready(function(){
